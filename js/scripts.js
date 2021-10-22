6722302f174ed2d49537e1b5dea0c059885bcadc
Kaxon <karenaxon@users.noreply.github.com>
2021-10-22 16:22:58 -0700

    Add styles to the language suggestion div.

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     const selection1 = $("#selection1").val();
     const selection2 = $("#selection2").val();
     const selection3 = $("#selection3").val();
+    
     $("#display-name").text(userName);
     $("#display-cSharp").hide();
     $("#display-ruby").hide();
@@ -14,7 +15,7 @@ $(document).ready(function() {
     $("#display-empty-form").hide();
 
 
-    if(selection1 === "0" && selection2 === "0" && selection3 === "0"){
+    if(selection1 === "0" && selection2 === "0" && selection3 === "0" || selection1 === "0" || selection2 === "0" || selection3 === "0"){
       $("#display-empty-form").show();
     } else if(selection1 === "2" && selection2 === "1" && selection3 === "1"){
       $("#display-cSharp").show();
