9adb28e83554d75328568bda87e57ca4ea8a2ff8
Kaxon <karenaxon@users.noreply.github.com>
2021-10-22 13:39:36 -0700

    Add variable to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 
 $(document).ready(function() {
-  $("suggester").submit(function(event) {
+  $("#suggester").submit(function(event) {
     event.preventDefault();
-
-
+    const userName = $("input#name-input").val();
+    const selection1 = $("#selection1").val();
+    const selection2 = $("#selection2").val();
+    const selection3 = $("#selection3").val();
+    
   });
 });
